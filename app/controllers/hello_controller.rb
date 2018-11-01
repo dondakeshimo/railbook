4a49dce8e3fdf948a73710ee86b1f372c6d8bc46
dondakeshimo <went.went.takkun135@gmail.com>
2018-11-01 12:39:47 +0900

    [add]scaffold

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,2 +1,13 @@
 class HelloController < ApplicationController
+  def index
+    render plain: 'こんにちは、世界!'
+  end
+
+  def view
+    @msg = 'Hello, World!'
+  end
+
+  def list
+      @books = Book.all
+  end
 end
